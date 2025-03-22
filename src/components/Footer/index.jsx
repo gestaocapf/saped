723f556638431a895d74551cd3e0b8dece3c0cad
Matheus Oliveira <gestaoensinandoatransgredir@gmail.com>
2025-03-22 16:56:33 -0300

    footer change

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="hidden bg-[#f2f0f0] h-[100px] mt-[200px]">
+        <footer className=" bg-white h-[100px] mt-[200px]">
             .
         </footer>
     )
