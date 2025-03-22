07feae97617f3b3bffc1b9ef1eb434624703e8e9
Matheus Oliveira <gestaoensinandoatransgredir@gmail.com>
2025-03-22 16:02:37 -0300

    fix: lord

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
+    MYSQL_PRISMA_URL: process.env.MYSQL_PRISMA_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  },
+};
 
 export default nextConfig;
