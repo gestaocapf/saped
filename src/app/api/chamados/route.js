6f1ad342c2f0ac20cc79a115815ab3ea60b2cdd3
Matheus Oliveira <gestaoensinandoatransgredir@gmail.com>
2025-03-22 18:50:33 -0300

    inspencionar token

--- src/app/api/chamados/route.js
+++ src/app/api/chamados/route.js
@@ -9,8 +9,10 @@ export async function GET(req) {
   try {
     const token = await getToken({
       req,
-      secret: process.env.JWT_SECRET, // Ensure JWT_SECRET is set in .env.local
+      secret: process.env.JWT_SECRET,
     });
+
+    console.log(token, "pooo");
 
     if (!token) {
       return NextResponse.json(
