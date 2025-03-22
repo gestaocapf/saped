c2974df9056b68c6249764e47974539405380e82
Matheus Oliveira <gestaoensinandoatransgredir@gmail.com>
2025-03-22 14:07:51 -0300

    fix - error l

--- src/app/api/auth/error/page.js
+++ src/app/api/auth/error/page.js
@@ -1,25 +1,33 @@
 "use client";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 
 export default function AuthErrorPage() {
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+
   return (
-    <div>
-      <Navbar />
-      <div className="flex flex-col items-center  justify-center mt-[200px]">
-        <h1 className="text-2xl font-bold">Erro de Autenticação</h1>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <Navbar />
+        <div className="flex flex-col items-center justify-center mt-[200px]">
+          <h1 className="text-2xl font-bold">Erro de Autenticação</h1>
 
-        <a
-          href="/login"
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-        >
-          Voltar para Login
-        </a>
+          <a
+            href="/login"
+            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          >
+            Voltar para Login
+          </a>
 
-        <p className="mt-2 text-sm w-[400px] text-center text-red-500">
-          Ocorreu um erro inesperado. Consulte o CAPF para obter mais
-          informações.
-        </p>
+          <p className="mt-2 text-sm w-[400px] text-center text-red-500">
+            {error === "AccessDenied"
+              ? "Você não tem permissão para acessar esta aplicação. Caso acredite que isso seja um erro, consulte o CAPF."
+              : "Ocorreu um erro inesperado."}
+          </p>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
